82a08f63092c7e6152201f51b9501f22027dbf3d
dgmarie <0001dgmarie@gmail.com>
2022-04-18 22:02:22 -0400

    use common QTI USB component
    Signed-off-by: pc1598 <piyushchouhan1598@gmail.com>

--- device.mk
+++ device.mk
@@ -376,7 +376,8 @@ PRODUCT_PACKAGES += \
     libjson
 
 TARGET_COMMON_QTI_COMPONENTS := \
-    adreno
+    adreno \
+    usb
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -435,14 +436,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
