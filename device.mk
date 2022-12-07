a11603ca04d5b8191c63f59ca465467c5f5fd0b1
pc1598 <piyushchouhan1598@gmail.com>
2022-12-07 19:12:56 +0000

    Use common QTI vibrator component

--- device.mk
+++ device.mk
@@ -369,7 +369,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
     bt \
     perf \
-    usb
+    usb \
+    vibrator
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -428,13 +429,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-      vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
